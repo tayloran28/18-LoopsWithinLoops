e9a87ca688a5d7ea8f4e19beda18f4be698f206d
tayloran <tayloran@rose-hulman.edu>
2019-01-17 11:13:20 -0500

    m4 done

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -3,17 +3,17 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Alyssa Taylor.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the other functions to test them. """
-    run_test_rectangle_of_stars()
-    run_test_triangle_of_stars()
-    run_test_decreasing_exclamation_marks()
-    run_test_alternating_brackets()
-    run_test_triangle_same_number_in_each_row()
+    # run_test_rectangle_of_stars()
+    # run_test_triangle_of_stars()
+    # run_test_decreasing_exclamation_marks()
+    # run_test_alternating_brackets()
+    # run_test_triangle_same_number_in_each_row()
     run_test_triangle_all_numbers_in_each_row()
 
 
@@ -43,8 +43,14 @@ def rectangle_of_stars(r, c):
        *****
     Preconditions:  r and c are non-negative integers.
     """
+
+    for k in range(r):
+        for j in range(c):
+            print('*', end='')
+        print()
+
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     #  *** Unless your instructor directs you otherwise,
@@ -97,8 +103,14 @@ def triangle_of_stars(r):
        *****
     Precondition:  r is a non-negative integer.
     """
+
+    for k in range(r):
+        for j in range(k+1):
+            print('*', end='')
+        print()
+
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     #  *** Unless your instructor directs you otherwise,
@@ -149,8 +161,14 @@ def decreasing_exclamation_marks(m, n):
        !!
     Precondition:  m and n are positive integers with m >= n.
     """
+
+    for k in range(m, n-1, -1):
+        for j in range(k):
+            print('!', end='')
+        print()
+
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -191,8 +209,17 @@ def alternating_brackets(m, n):
        []
     Precondition:  m and n are positive integers with m >= n.
     """
+
+    for k in range(m, n-1, -1):
+        for j in range(k):
+            if j % 2 == 0:
+                print('[', end='')
+            else:
+                print(']', end='')
+        print()
+
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -234,8 +261,14 @@ def triangle_same_number_in_each_row(r):
        55555
     Precondition:  r is a non-negative integer.
     """
+
+    for k in range(r+1):
+        for j in range(k):
+            print(k, end='')
+        print()
+
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -277,8 +310,14 @@ def triangle_all_numbers_in_each_row(r):
        12345
     Precondition:  r is a non-negative integer.
     """
+
+    for k in range(r):
+        for j in range(k+1):
+            print(j+1, end='')
+        print()
+
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
--- src/m4_nested_loops_in_sequences.py
+++ src/m4_nested_loops_in_sequences.py
@@ -3,15 +3,15 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of SEQUENCES OF SUB-SEQUENCES.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Alyssa Taylor.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
     """ Calls the other functions to test them. """
-    run_test_multiply_numbers()
-    run_test_sum_numbers()
-    run_test_print_characters()
+    # run_test_multiply_numbers()
+    # run_test_sum_numbers()
+    # run_test_print_characters()
     run_test_print_characters_slanted()
 
 
@@ -116,8 +116,13 @@ def multiply_numbers(sequence_of_lists):
        and the elements of the lists can be multiplied by an integer.
        [FYI: This 'can be multiplied ...' is an example of DUCK TYPING.]
     """
+    for k in range(len(sequence_of_lists)):
+        a = sequence_of_lists[k]
+        for j in range(len(a)):
+            a[j] = a[j] * (k+1)
+
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
@@ -133,7 +138,7 @@ def multiply_numbers(sequence_of_lists):
 def run_test_sum_numbers():
     """ Tests the    sum_numbers    function. """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this TEST function.
+    # DONE: 3. Implement this TEST function.
     #   It TESTS the  sum_numbers  function defined below.
     #   Include at least **   3   ** tests (we wrote two for you).
     # ------------------------------------------------------------------
@@ -154,6 +159,21 @@ def run_test_sum_numbers():
 
     # TO DO 3 (continued): Add your ADDITIONAL test(s) here:
 
+    # Test 3:
+    expected = 9
+    answer = sum_numbers([(1, 1, 1), (1, 1), [1, 1, 1, 1]])
+    print('Expected and actual are:', expected, answer)
+
+    # Test 4:
+    expected = 46
+    answer = sum_numbers([(9, 1, 0), (10, 20), [1, 2, 3]])
+    print('Expected and actual are:', expected, answer)
+
+    # Test 5:
+    expected = 35
+    answer = sum_numbers([(7, 1, 8), [10], [2, 3, 4]])
+    print('Expected and actual are:', expected, answer)
+
 
 def sum_numbers(seq_seq):
     """
@@ -165,8 +185,16 @@ def sum_numbers(seq_seq):
     Preconditions:  the given argument is a sequences of sequences,
                     and each item in the subsequences is a number.
     """
+
+    total = 0
+    for k in range(len(seq_seq)):
+        seq = seq_seq[k]
+        for j in range(len(seq)):
+            total = total + seq[j]
+    return total
+
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
 
@@ -216,8 +244,14 @@ def print_characters(sequence_of_strings):
        !
     Precondition:  the given argument is a sequence of strings.
     """
+
+    for k in range(len(sequence_of_strings)):
+        seq_str = sequence_of_strings[k]
+        for j in range(len(seq_str)):
+            print(seq_str[j])
+
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     # ------------------------------------------------------------------
@@ -269,8 +303,16 @@ def print_characters_slanted(sequence_of_strings):
             !
     Precondition:  the given argument is a sequence of strings.
     """
+
+    for k in range(len(sequence_of_strings)):
+        seq_str = sequence_of_strings[k]
+        space = ' '
+        for j in range(len(seq_str)):
+            print(space + seq_str[j])
+            space = space + ' '
+
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
